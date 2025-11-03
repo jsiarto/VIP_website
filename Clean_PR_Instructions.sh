ec7f60ed1c42b7d1f20337d27223d494c069ce8e
Dirk Colbry <colbrydi@msu.edu>
2025-11-03 14:04:08 -0500

    Updating instructions to include documentation

--- Clean_PR_Instructions.sh
+++ Clean_PR_Instructions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Command used by Dirk to sync this repository with Tasmia's 
 git checkout -b pr-clean
 git checkout upstream/main -- _config.yml
 git commit -am "Revert _config.yml to match upstream for clean PR"
